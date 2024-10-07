3dba3ae00a92af28391c558d77f150ff2180862f
Marcus0086 <guptamarcus42@gmail.com>
2024-10-07 15:50:20 +0530

    Refactor main.go to gracefully shutdown server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"etl/pkg/db"
 	"etl/routes"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
@@ -11,6 +16,10 @@ import (
 
 func main() {
 	app := pocketbase.New()
+	err := app.Bootstrap()
+	if err != nil {
+		log.Fatal(err)
+	}
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		return db.SetupSchema(app)
 	})
@@ -19,8 +28,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		if err := app.Start(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	log.Println("Shutting down gracefully...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := app.ResetBootstrapState(); err != nil {
+		log.Fatalf("PocketBase graceful shutdown failed: %v", err)
 	}
+
+	log.Println("Server gracefully stopped.")
 
 }
